d70782562b0e3c86e8975538b459694735783fc2
raul149 <72709781+raul149@users.noreply.github.com>
2026-03-20 17:45:51 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/ping")
+def read_ping():
+    return {"message": "pong!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
